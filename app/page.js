93b99f0f49e3aee708f2cb2dc9c242d00edf6acc
br adithya <bradithya945@gmail.com>
2024-08-18 21:37:34 +0530

    added normal function for page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
-const page = () => {
-  return (
-    <div>page</div>
-  )
+// // First class function
+// const page = () => {
+//   return (
+//     <div>page</div>
+//   )
+// }
+
+function page(){
+  return <h1>Hello</h1>
 }
 
 export default page
